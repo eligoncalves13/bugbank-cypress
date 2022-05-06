263bbc1440c99c1623b4df29445b7bd3e5202cd9
Élida <elidabeatriz13@gmail.com>
2022-05-06 00:48:49 -0300

    refactor with page object

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,4 +1,4 @@
-import { registerPage } from "../../support/pages/RegisterPage.po";
+import { registerPage } from "../support/pages/RegisterPage.po";
 
 describe("Teste do Cadastro do site BugBank", () => {
     const login = () => {
@@ -8,128 +8,100 @@ describe("Teste do Cadastro do site BugBank", () => {
             cy.contains("button", "Acessar").click();
         });
     };
-    const cadastrarSemSaldo = () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-    };
-
+    
     beforeEach(() => {
-        cy.visit("");
-        cy.contains("button", "Registrar").click();
+        registerPage.visitar();
+        registerPage.abrirFormularioCadastro();
     }); 
 
     it("Os campos nome, email, senha e confirmar senha são de preenchimento obrigatório", () => {
-        cy.contains("button", "Cadastrar").click({ force: true });
-        cy.get(".card__register .input__warging:contains('É campo obrigatório')").should('have.length', 4);
+        registerPage.confirmarFormulario();
+        registerPage.verficarMensagemDeCamposObrigatorios();
     });
 
     it("O campo nome não pode ser vazio", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", "Nome não pode ser vazio").should("be.visible");
+        registerPage.preencherEmail("raro@raro.com");
+        registerPage.preencherSenha("1234");
+        registerPage.preencherConfirmarSenha("1234");
+        registerPage.confirmarFormulario();
+        registerPage.verificarModalDeCampoVazio("Nome");
     });
 
     it("O campo email não pode ser vazio", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", "Email não pode ser vazio").should("be.visible"); 
+        registerPage.preencherNome("Raro");
+        registerPage.preencherSenha("1234");
+        registerPage.preencherConfirmarSenha("1234");
+        registerPage.confirmarFormulario();
+        registerPage.verificarModalDeCampoVazio("Email");
     });
 
     it("O campo senha não pode ser vazio", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", "Senha não pode ser vazio").should("be.visible");
+        registerPage.preencherEmail("raro@raro.com");
+        registerPage.preencherNome("Raro");
+        registerPage.preencherConfirmarSenha("1234");    
+        registerPage.confirmarFormulario();
+        registerPage.verificarModalDeCampoVazio("Senha");
     });
 
     it("O campo confirmar senha não pode ser vazio", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", "Confirmar senha não pode ser vazio").should("be.visible");
+        registerPage.preencherEmail("raro@raro.com");
+        registerPage.preencherNome("Raro");
+        registerPage.preencherSenha("1234");
+        registerPage.confirmarFormulario();
+        registerPage.verificarModalDeCampoVazio("Confirmar senha");
     });
 
     it("Deve ser possível criar conta com saldo", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.get("#toggleAddBalance").click({ force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.get("#btnCloseModal").click();
+        registerPage.preencherFormulario("raro@raro.com", "Raro", "1234", "1234");
+        registerPage.adicionarSaldo();
+        registerPage.confirmarFormulario();
+        registerPage.fecharModalDeContaCriada("#btnCloseModal", "Fechar");
 
         login();
-        cy.contains("#textBalance", "Saldo em conta R$ 1.000,00");     
+        registerPage.verificarTextoDeSaldo("1.000,00");
+
     });
 
     it("Deve ser possível criar conta sem saldo", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.get("#btnCloseModal").click();
+        registerPage.preencherFormulario("raro@raro.com", "Raro", "1234", "1234");
+        registerPage.confirmarFormulario();
+        registerPage.fecharModalDeContaCriada("#btnCloseModal", "Fechar");
 
         login();
-        cy.contains("#textBalance", "Saldo em conta R$ 0,00");     
+        registerPage.verificarTextoDeSaldo("0,00");   
     });
 
     it("Não deve ser possível preencher os campos senha e confirmação de senha com dados diferentes", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("123", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", "As senhas não são iguais.").should("be.visible");  
+        registerPage.preencherEmail("raro@raro.com");
+        registerPage.preencherNome("Raro");
+        registerPage.preencherSenha("1234");
+        registerPage.preencherConfirmarSenha("12345");  
+        registerPage.confirmarFormulario();
+        registerPage.verificarModalDeSenhasDiferentes();
     });
 
     it("Deve ser possível visualizar número da conta ao ser criada com sucesso", () => {
-        cy.get(".card__register").within(() => {
-            cy.get("input[name='email']").type("raro@raro.com", { force: true });
-            cy.get("input[name='name']").type("Raro", { force: true });
-            cy.get("input[name='password']").type("1234", { force: true });
-            cy.get("input[name='passwordConfirmation']").type("1234", { force: true });
-            cy.contains("button", "Cadastrar").click({ force: true });
-        });
-        cy.contains("#modalText", /A conta [0-9][0-9][0-9]-[0-9] foi criada com sucesso/).should("be.visible"); 
+        registerPage.preencherEmail("raro@raro.com");
+        registerPage.preencherNome("Raro");
+        registerPage.preencherSenha("1234");
+        registerPage.preencherConfirmarSenha("1234");  
+        registerPage.confirmarFormulario();
+        registerPage.verificarModaldeContaCriada();
     });
 
     it("Deve ser possível fechar o modal de cadastro com sucesso com o botão fechar do header", () => {
-        cadastrarSemSaldo();
-        cy.contains("#btnCloseModal", "Fechar").should("be.visible");
-        cy.contains("#btnCloseModal", "Fechar").click();
-        cy.contains("#btnCloseModal", "Fechar").should("not.exist");
+        registerPage.preencherFormulario("raro@raro.com", "Raro", "1234", "1234");
+        registerPage.confirmarFormulario();
+        registerPage.verificarVisibilidadeBotaoFecharModal("a[href='#']", "x")
+        registerPage.fecharModalDeContaCriada("a[href='#']", "x");
+        registerPage.verificarExistenciaBotaoFecharModal("a[href='#']", "x");
     });
     
-    it.only("Deve ser possível fechar o modal de cadastro com sucesso com o botão fechar do footer", () => {
-        cadastrarSemSaldo();
-        cy.contains("#btnCloseModal", "Fechar").should("be.visible");
-        cy.contains("#btnCloseModal", "Fechar").click();
-        cy.contains("#btnCloseModal", "Fechar").should("not.exist");
+    it("Deve ser possível fechar o modal de cadastro com sucesso com o botão fechar do footer", () => {
+        registerPage.preencherFormulario("raro@raro.com", "Raro", "1234", "1234");
+        registerPage.confirmarFormulario();
+        registerPage.verificarVisibilidadeBotaoFecharModal("#btnCloseModal", "Fechar")
+        registerPage.fecharModalDeContaCriada("#btnCloseModal", "Fechar");
+        registerPage.verificarExistenciaBotaoFecharModal("#btnCloseModal", "Fechar");
     });
 });
--- cypress/support/pages/RegisterPage.po.js
+++ cypress/support/pages/RegisterPage.po.js
@@ -1,5 +1,84 @@
 class RegisterPage {
+    formularioDeRegistro = ".card__register";
+    inputEmail = "input[name='email']";
+    inputNome = "input[name='name']";
+    inputSenha = "input[name='password']";
+    inputConfirmarSenha = "input[name='passwordConfirmation']";
+    toggleAdicionarSaldo = "#toggleAddBalance";
 
+    visitar() {
+        cy.visit("");
+    };
+
+    abrirFormularioCadastro() {
+        cy.contains("button", "Registrar").click();
+    };
+
+    preencherFormulario(email, nome, senha, confirmarSenha) {
+        // cy.get(this.formularioDeRegistro).within(() => {
+            this.preencherEmail(email);
+            this.preencherNome(nome);
+            this.preencherSenha(senha);
+            this.preencherConfirmarSenha(confirmarSenha);
+        // });
+    };
+
+    preencherEmail(email) {
+        cy.get(this.formularioDeRegistro + " " + this.inputEmail).type(email, { force: true });
+    };
+
+    preencherNome(nome) {
+        cy.get(this.formularioDeRegistro + " " + this.inputNome).type(nome, { force: true });
+    };
+
+    preencherSenha(senha) {
+        cy.get(this.formularioDeRegistro + " " + this.inputSenha).type(senha, { force: true });
+    };
+
+    preencherConfirmarSenha(confirmarSenha) {
+        cy.get(this.formularioDeRegistro + " " + this.inputConfirmarSenha).type(confirmarSenha, { force: true });
+    };
+
+    confirmarFormulario() {
+        cy.contains("button", "Cadastrar").click({ force: true });
+    };
+
+    adicionarSaldo(){
+        cy.get(this.toggleAdicionarSaldo).click({ force: true });
+    }
+
+    verficarMensagemDeCamposObrigatorios(){
+        cy.get(".card__register .input__warging:contains('É campo obrigatório')").should('have.length', 4);
+    };
+
+    verificarModalDeCampoVazio(campo){
+        cy.contains("#modalText", `${campo} não pode ser vazio`).should("be.visible");
+    };
+
+    fecharModalDeContaCriada(elemento, texto){
+        cy.contains(elemento, texto).click();
+    };
+
+    verificarTextoDeSaldo(saldo){
+        cy.contains("#textBalance", `Saldo em conta R$ ${saldo}`);     
+    };
+
+    verificarModalDeSenhasDiferentes(){
+        cy.contains("#modalText", "As senhas não são iguais.").should("be.visible");  
+    }
+
+    verificarModaldeContaCriada(){
+        cy.contains("#modalText", /A conta [0-9][0-9][0-9]-[0-9] foi criada com sucesso/).should("be.visible"); 
+    }
+
+    verificarVisibilidadeBotaoFecharModal(elemento, texto){
+        cy.contains(elemento, texto).should("be.visible");
+    };
+
+    verificarExistenciaBotaoFecharModal(elemento, texto){
+        cy.contains(elemento, texto).should("not.exist");
+    };
 };
+
 
 export var registerPage = new RegisterPage();
